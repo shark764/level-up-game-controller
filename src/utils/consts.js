0934410ebbcffa23369b84b0c18ad74a59de86dd
Farid Hernandez <farid.hdz.64@gmail.com>
2021-07-18 21:18:39 -0600

    Moving http and socket to same port - Heroku only allows one port open

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -1,10 +1,13 @@
 export const whiteList = [
   /(localhost|127.0.0.1)./,
-  'https://level-up-game-controller-gateway.herokuapp.com',
+  'http://level-up-game-controller.herokuapp.com',
+  'http://level-up-reporting.herokuapp.com',
+  'https://level-up-game-controller.herokuapp.com',
+  'https://level-up-reporting.herokuapp.com',
   'https://react-socket-io-client-1.herokuapp.com',
   'https://react-socket-io-client-1.netlify.app',
-  'http://level-up-game-controller.herokuapp.com',
-  'https://level-up-game-controller.herokuapp.com',
   'ws://level-up-game-controller.herokuapp.com',
+  'ws://level-up-reporting.herokuapp.com',
   'wss://level-up-game-controller.herokuapp.com',
+  'wss://level-up-reporting.herokuapp.com',
 ];
